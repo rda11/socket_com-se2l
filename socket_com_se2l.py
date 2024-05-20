3809a9e94f1825d8016b95c3459175d89c5da8b5
Rajab deen ali <99082327+rda11@users.noreply.github.com>
2024-05-20 09:44:23 +0530

    Add files via upload

---
+++ socket_com_se2l.py
@@ -0,0 +1,25 @@
+import socket
+
+#create socket connection
+c = socket.socket()
+c.connect(('192.168.0.10',10940))
+
+#send/receive through socket
+sent_bytes = c.send(b'000EAR00A012\n')
+output,addr = c.recvfrom(5000)
+val = str(output.decode('ascii'))
+
+#Assigning error codes to variables
+error_status = val[14]
+error_code = val[15:17]
+
+#print values
+if error_status == '1':
+    print('Error status: Detected')
+else:
+    print('Error status: Not detected')
+    
+if error_code =='00':
+    print('Error code: Nil')
+else:
+    print('Error code:', (int(error_code)+40))
